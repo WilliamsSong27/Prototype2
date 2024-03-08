a957ed5d5696bdb4685dd450b9d6839dc34a75e3
WilliamsSong27 <156847087+WilliamsSong27@users.noreply.github.com>
2024-03-08 05:24:43 -0700

    Add files via upload

---
+++ SpawnManagerX.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+    public GameObject[] dogSpawn;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Spawndog();
+        }
+    }
+
+    void Spawndog()
+    {
+        Vector3 spawnPosition = new Vector3(-35, 0, 0);
+
+        Instantiate(dogSpawn[0], spawnPosition, dogSpawn[0].transform.rotation);
+    }
+
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+}
